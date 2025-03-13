fa5e38f186b358564c8c9a3baedcbbed347d0dfe
gerigerdev <publiger29@gmail.com>
2025-03-13 23:37:21 +0100

    ex1-init

--- src/main/java/org/exercice1/Main.java
+++ src/main/java/org/exercice1/Main.java
@@ -1,7 +1,22 @@
 package org.exercice1;
 
+import org.utils.FileUtils;
+import org.utils.SortOrder;
+
+import java.io.File;
+
 public class Main {
+    static final String resourcesPath = "/src/main/resources";
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+        readAllFilesFromDir(FileUtils.getDirPath(resourcesPath));
     }
+
+    public static void readAllFilesFromDir(String dirName){
+        File[] files = FileUtils.getAllFilesFromDir(FileUtils.getDirPath(resourcesPath));
+        FileUtils.sortFiles(files, SortOrder.DESC);
+        for(String file : FileUtils.convertFilesToArray(files)){
+            System.out.println(file);
+        }
+    }
+
 }
